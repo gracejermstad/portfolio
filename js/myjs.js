b3f274457e4ed8a07e135f06d1a7966c35261a81
gracejermstad <143536363+gracejermstad@users.noreply.github.com>
2023-12-03 11:48:40 -0500

    Add files via upload

---
+++ js/myjs.js
@@ -0,0 +1,37 @@
+    window.addEventListener('load', function() {
+        document.querySelectorAll('.slide-in').forEach(function(element) {
+            element.classList.add('slide-in');
+        });
+    });
+    const slides = document.querySelectorAll('.carousel-slide');
+let currentSlide = 0;
+
+function showSlide(n) {
+    slides[currentSlide].style.display = 'none';
+    currentSlide = (n + slides.length) % slides.length;
+    slides[currentSlide].style.display = 'block';
+}
+
+// Click event for each individual page
+document.querySelectorAll('.carousel-slide img').forEach((img, index) => {
+    img.addEventListener('click', () => {
+        if (index % 2 === 0) {
+            // Clicked on the first image of a pair, go to the previous pair
+            showSlide(currentSlide - 2);
+        } else {
+            // Clicked on the second image of a pair, go to the next pair
+            showSlide(currentSlide + 2);
+        }
+    });
+});
+
+document.querySelector('.carousel-prev').addEventListener('click', () => {
+    showSlide(currentSlide - 2);
+});
+
+document.querySelector('.carousel-next').addEventListener('click', () => {
+    showSlide(currentSlide + 2);
+});
+
+    
+
